52835c4a988ae368f7c4f77d0a2a0e55ffc50710
Arash <moosapour.arash@gmail.com>
2022-04-05 18:43:24 +0430

    Save subscriptions to Database

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
 use std::net::TcpListener;
+use sqlx::{Connection, PgPool};
 use zero::configuration::get_configuration;
 use zero::startup::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
-// We have removed the hard-coded `8000` - it's now coming from our settings!
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
